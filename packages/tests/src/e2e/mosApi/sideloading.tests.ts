fefccb2bb1c4abf9f89b973f404e253b5ec7caed
Huajie Zhang <huajiezhang@microsoft.com>
2025-02-11 15:29:11 +0800

    test: add test case id for mos api e2e (#13163)

--- packages/tests/src/e2e/mosApi/sideloading.tests.ts
+++ packages/tests/src/e2e/mosApi/sideloading.tests.ts
@@ -11,31 +11,35 @@ import { M365TitleHelper } from "../../commonlib/m365TitleHelper";
 import { assert } from "chai";
 
 describe("MOS3 API", function () {
-  it(`sideloading`, { author: "huajiezhang@microsoft.com" }, async function () {
-    const m365TitleHelper = await M365TitleHelper.init(
-      "https://titles.prod.mos.microsoft.com",
-      "https://titles.prod.mos.microsoft.com/.default"
-    );
-    const packageFilePath = path.join(__dirname, ".", "appPackage.local.zip");
-    let error: any;
-    let success = false;
-    try {
-      const res = await m365TitleHelper.acquire(packageFilePath);
-      assert.isDefined(res[0], res[1]);
-      await m365TitleHelper.unacquire(res[0]);
-      success = true;
-    } catch (e) {
-      console.error(`Failed to call MOS3 API (acquire/unacquire): ${e}`);
-      error = e;
-    }
-    if (success) {
-      console.log("Successfully call MOS3 API (acquire/unacquire)");
-    } else {
-      console.error(
-        "Failed to call MOS3 API (acquire/unacquire) after 5 retries"
+  it(
+    "install & uninstall",
+    { testPlanCaseId: 31339285, author: "huajiezhang@microsoft.com" },
+    async function () {
+      const m365TitleHelper = await M365TitleHelper.init(
+        "https://titles.prod.mos.microsoft.com",
+        "https://titles.prod.mos.microsoft.com/.default"
       );
-      console.error(error);
+      const packageFilePath = path.join(__dirname, ".", "appPackage.local.zip");
+      let error: any;
+      let success = false;
+      try {
+        const res = await m365TitleHelper.acquire(packageFilePath);
+        assert.isDefined(res[0], res[1]);
+        await m365TitleHelper.unacquire(res[0]);
+        success = true;
+      } catch (e) {
+        console.error(`Failed to call MOS3 API (acquire/unacquire): ${e}`);
+        error = e;
+      }
+      if (success) {
+        console.log("Successfully call MOS3 API (acquire/unacquire)");
+      } else {
+        console.error(
+          "Failed to call MOS3 API (acquire/unacquire) after 5 retries"
+        );
+        console.error(error);
+      }
+      assert.isTrue(success);
     }
-    assert.isTrue(success);
-  });
+  );
 });
